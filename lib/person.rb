3fbf1142742cc75ee9e566030dc64ba81f27ed2d
Jesse Brown <Brownjesse8895@yahoo.com>
2022-02-22 05:32:47 -0600

    commit tree 7f2cf6fbcc0a118798c4826c411e348a1e42c99a

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
+require 'pry' 
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+      binding.pry
+    end
+  end
 end
+
